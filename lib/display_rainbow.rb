07b032c33e5d4cb4d69e501ba8c60782dcc27b13
Robert Tate <btate712@gmail.com>
2019-03-08 14:11:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,8 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
+  output_string = ""
   colors.each do |color|
-    puts "#{color.first.upcase}: #{color}"
+    output_string += "#{color[0].upcase}: #{color}"
   end
+  puts output_string
 end
